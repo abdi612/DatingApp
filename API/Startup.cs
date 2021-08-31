6f559760daae3667572bda86792bfbe2282f27c5
Abdi Ali <abdialidev@gmail.com>
2021-08-30 21:15:57 -0500

    angular api call

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,8 +26,6 @@ namespace API
 
         }
 
-        //public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -39,26 +37,29 @@ namespace API
             });
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+        // where you please code or order method is very important in configure method
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            // fix for XMLHttpRequest error when connecting call api from angular
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+                        .WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
